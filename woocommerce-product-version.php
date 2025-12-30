f3241b27e8c6cded261dd1b462c5279d2e0b65a0
Michael Fisher <880111+mfisher31@users.noreply.github.com>
2025-12-30 09:02:29 -0500

    Update author email from ProtonMail to Gmail

--- woocommerce-product-version.php
+++ woocommerce-product-version.php
@@ -7,7 +7,7 @@
  * Requires at least: 5.8
  * Requires PHP: 7.4
  * Author: Michael Fisher
- * Author URI: mailto:mfisher31@protonmail.com
+ * Author URI: mailto:mfisher31@gmail.com
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: woocommerce-product-version
